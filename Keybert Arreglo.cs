af94d3ac9daf18a4c21dc2a23732200e7fabbc35
keybertr <82736829+keybertr@users.noreply.github.com>
2021-10-15 21:34:31 -0400

    Add files via upload

---
+++ Keybert Arreglo.cs
@@ -0,0 +1,17 @@
+﻿using System; 
+class MainClass {
+  public static void Main (string[] args) {
+		// Nuestro arreglo
+		double[] valores = {90, 98, 85, 95, 90, 80, 70, 100, 55};
+		// Vamos a almacenar la sumatoria
+		double sumatoria = 0;
+		// Lo recorremos con un foreach
+		foreach(double valor in valores){
+			// Ir agregando el valor a la sumatoria
+			sumatoria += valor;
+		}
+		// la media resulta de dividir la sumatoria entre la cantidad de elementos
+		double media = sumatoria / valores.Length;
+    Console.WriteLine(" La Media de los Valores es: " + media);
+  }
+}
