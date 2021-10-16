f291f195765bac7c7b577ec9902d69096db25370
keybertr <82736829+keybertr@users.noreply.github.com>
2021-10-15 21:29:31 -0400

    Add files via upload

---
+++ Keybert Cadena.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Cadenas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" Escriba una frase: ");
+            
+            String cadena = Console.ReadLine();
+            String[] palabras = cadena.Split();
+            for (int i = 0; i < palabras.Length; i++)  
+            {
+                Console.WriteLine(palabras[i]);
+
+            }    
+        }
+    }
+}
